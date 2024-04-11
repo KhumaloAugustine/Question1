b73e1834373b5ee45b645842d82cbacfd141ed81
augustinekhumalo <mr.a.s.khumalo@gmail.com>
2024-04-11 10:38:37 +0200

    Added comment on Main.java for readability

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@ import main.com.library.controllers.LibraryController;
 
 public class Main {
 
+    //  The main entry point of the library system application
     public static void main(String[] args) {
+        //  Create a LibraryController object to handle the application logic
         LibraryController controller = new LibraryController();
+        //  Start the main program loop controlled by the LibraryController
         controller.run();
     }
 }
